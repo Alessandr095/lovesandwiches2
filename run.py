b273d6b130c954e5f63f622b9c0a0fe5ad628594
Alessandro De Siena <a.desiena@icloud.com>
2024-10-16 17:01:42 +0000

    calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -60,7 +60,7 @@ def update_worksheet(data, worksheet):
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f" {worksheet} worksheet updated successfully.\n")
-    
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -80,6 +80,27 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entries_sales():
+    sales = SHEET.worksheet('sales')
+    
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+def calculate_stock_data(data):
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 
 def main():
     """
@@ -90,7 +111,8 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
-
-
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 print("Welcome to Love Sandwiches Data Automation")
 main()
